c27fcb275f2f22997f709342306d83d549df3586
simonHuang12 <simonh8@nycstudents.net>
2023-10-09 00:39:41 -0400

    AAAAAAAAAAAAAAAAAAAAAAAA

--- script.js
+++ script.js
@@ -1,56 +1,78 @@
-const symbols = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
-const cards = symbols.concat(symbols); // Duplicate the symbols to create pairs
-let openedCards = [];
-let matchedPairs = 0;
-let moves = 0;
+const images = [
+    'img1.png',
+    'img2.png',
+    'img3.png',
+    'img4.png',
+    'img5.png',
+    'img6.png',
+    'img7.png',
+    'img8.png',
+    'img1.png',
+    'img2.png',
+    'img3.png',
+    'img4.png',
+    'img5.png',
+    'img6.png',
+    'img7.png',
+    'img8.png',
+];
 
-const container = document.getElementById('game-container');
+const gameContainer = document.querySelector('.game-container');
+let cards = [];
+let flippedCards = [];
+let matches = 0;
+let score = 0;
 
-function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+// Shuffle the images
+images.sort(() => Math.random() - 0.5);
+
+// Create card elements and add to the game container
+images.forEach((img) => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.dataset.img = img; // Store the image filename as a data attribute
+    card.innerHTML = `<img src="images/back.png" alt="Card Back">`;
+    card.addEventListener('click', () => flipCard(card));
+    gameContainer.appendChild(card);
+    cards.push(card);
+});
+
+function flipCard(card) {
+    score++;
+    document.getElementById("score").innerHTML = score;
+    if (flippedCards.length < 2 && !flippedCards.includes(card) && !card.classList.contains('hidden')) {
+        card.innerHTML = `<img src="images/${card.dataset.img}" alt="Card">`;
+        flippedCards.push(card);
+        if (flippedCards.length === 2) {
+            setTimeout(checkMatch, 1000);
+        }
     }
 }
 
-function createCard(symbol) {
-    const card = document.createElement('div');
-    card.classList.add('card');
-    card.textContent = symbol;
-    card.addEventListener('click', () => {
-        if (openedCards.length < 2 && !openedCards.includes(card) && !card.classList.contains('matched')) {
-            card.classList.add('flipped');
-            openedCards.push(card);
-            if (openedCards.length === 2) {
-                moves++;
-                document.getElementById('moves').textContent = moves;
-                if (openedCards[0].textContent === openedCards[1].textContent) {
-                    openedCards.forEach(card => card.classList.add('matched'));
-                    matchedPairs++;
-                    openedCards = [];
-                    if (matchedPairs === symbols.length) {
-                        alert(`Congratulations! You won in ${moves} moves.`);
-                    }
-                } else {
-                    setTimeout(() => {
-                        openedCards.forEach(card => card.classList.remove('flipped'));
-                        openedCards = [];
-                    }, 1000);
-                }
-            }
+function checkMatch() {
+    
+    const [card1, card2] = flippedCards;
+    const img1 = card1.dataset.img;
+    const img2 = card2.dataset.img;
+
+    if (img1 === img2) {
+        card1.removeEventListener('click', () => flipCard(card1));
+        card2.removeEventListener('click', () => flipCard(card2));
+        flippedCards = [];
+        matches++;
+        document.getElementById("matched-pairs").innerHTML = matches;
+
+        if (matches === images.length / 2) {
+            setTimeout(() => alert('Congratulations! You won!'), 500);
+            resetGame();
         }
-    });
-    return card;
+    } else {
+        card1.innerHTML = `<img src="images/back.png" alt="Card Back">`;
+        card2.innerHTML = `<img src="images/back.png" alt="Card Back">`;
+        flippedCards = [];
+    }
 }
 
-shuffle(cards);
-
-for (const symbol of cards) {
-    const card = createCard(symbol);
-    container.appendChild(card);
+function resetGame(){
+    location.reload();
 }
-
-const movesElement = document.createElement('p');
-movesElement.textContent = 'Moves: 0';
-movesElement.id = 'moves';
-container.insertAdjacentElement('beforebegin', movesElement);
